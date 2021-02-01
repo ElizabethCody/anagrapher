3b536c6227b18f23c589d0811d7b1bc3c4ea5641
Maxwell Cody <maxwell@cody.sh>
2021-01-31 21:09:49 -0600

    cli: Improved help functionality.

--- src/main/groovy/sh/cody/anagrapher/Anagrapher.groovy
+++ src/main/groovy/sh/cody/anagrapher/Anagrapher.groovy
@@ -5,7 +5,7 @@ import groovy.cli.picocli.CliBuilder
 class Anagrapher {
    static void main(String... args) {
       def cli = new CliBuilder(name: 'anagrapher', header: 'Construct and solve anagrams from a graph.')
-      cli.h(longOpt: 'help', 'show this message')
+      cli.h(longOpt: 'help', 'show this message and exit')
       cli.d(argName: 'depth', type: int, longOpt: 'depth', required: true, defaultValue: '4', args: 1, 'set graph traversal depth')
       cli.i(argName: 'true/false', type: boolean, longOpt: 'insensitivity', required: true, defaultValue: 'true', args: 1, 'set case insensitivity')
       cli.t(argName: 'threads', type: int, longOpt: 'threads', required: false, args: 1, 'force worker thread count')
@@ -14,6 +14,7 @@ class Anagrapher {
 
       if(options.h) {
          cli.usage()
+         return
       }
    }
 }
